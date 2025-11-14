1947185ab1c8c561837d5f21419d39c6af2f8201
Yufeng Xu <yx3038@nyu.edu>
2025-11-14 00:00:15 -0500

    added reinit script

--- pruning/wanda/scripts/prune_llama2-7b.sh
+++ pruning/wanda/scripts/prune_llama2-7b.sh
@@ -32,6 +32,8 @@ sparsities=(
     4:8
 )
 
+ratio=0.5
+
 PROJ_DIR=$(pwd)
 export PYTHONPATH=${PROJ_DIR}/src/lib:${PYTHONPATH:-''}
 export PYTHONPATH=${PROJ_DIR}/lm-evaluation-harness:${PYTHONPATH}
@@ -44,14 +46,14 @@ log_dir=${PROJ_DIR}/outputs
 cd $PROJ_DIR/src
 for method in "${methods[@]}"; do
     for sparsity in "${sparsities[@]}"; do
-        echo "[INFO] Pruning with method: $method and sparsity: $sparsity"
+        echo "[INFO] Pruning with method: $method and sparsity: $sparsity and ratio: $ratio"
         python main.py \
             --model ${model_path} \
             --prune_method ${method} \
-            --sparsity_ratio 0.5 \
+            --sparsity_ratio ${ratio} \
             --sparsity_type ${sparsity} \
             --save ${log_dir}/${method}/${sparsity}/ \
-            --save_model ${save_dir}/${method}/${model_name}_${method}_${sparsity} \
+            --save_model ${save_dir}/${method}/${model_name}_${method}_${sparsity}_${ratio} \
             --eval_zero_shot
     done
 done
